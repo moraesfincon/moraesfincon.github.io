8795422c948b39cddd2136badedd3b4b0b30956a
Diego Collares <diego.collares@gmail.com>
2016-10-04 14:07:43 -0300

    generate model Banner

---
+++ db/migrate/20161004170708_create_banners.rb
@@ -0,0 +1,12 @@
+class CreateBanners < ActiveRecord::Migration[5.0]
+  def change
+    create_table :banners do |t|
+      t.string :title
+      t.text :description
+      t.string :image
+      t.boolean :public
+
+      t.timestamps
+    end
+  end
+end
