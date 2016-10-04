7ca39318b1c804f13022c5e4bb559c01f910b909
Diego Collares <diego.collares@gmail.com>
2016-10-04 16:22:40 -0300

    create route ckeditor

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+
+  mount Ckeditor::Engine => '/ckeditor'
+
   devise_for :admin_users, ActiveAdmin::Devise.config
+
   ActiveAdmin.routes(self)
+
   get 'page/index'
   root 'page#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
 end
