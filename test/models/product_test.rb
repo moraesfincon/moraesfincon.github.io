700a2181583aec8bfe051d3bbfa00a245e922c8c
Diego Collares <diego.collares@gmail.com>
2016-10-04 13:39:04 -0300

    create test files

---
+++ test/models/product_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class ProductTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+  test "should not save product without name" do
+    product = Product.new
+    assert_not product.save
+  end
+end
