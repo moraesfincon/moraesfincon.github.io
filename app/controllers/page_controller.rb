3e252b0aa4453a8af160484c8a34d7e025985205
Diego Collares <diego.collares@gmail.com>
2016-10-04 16:19:32 -0300

    create variable page index

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,5 +1,9 @@
 class PageController < ApplicationController
+
   def index
+  	@products_main = Product.where(main: true).take(1)
   	@products = Product.where(public: true).take(6)
+    @banners  = Banner.where(public: true)
   end
+
 end
