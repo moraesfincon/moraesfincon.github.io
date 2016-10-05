6bb6964d50261424cc5f7dbb083efbda7cea3519
Diego Collares <diego.collares@gmail.com>
2016-10-05 18:58:44 -0300

    adicionando variavel @newsletter

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,9 +1,16 @@
 class PageController < ApplicationController
 
+  before_action :new
+
   def index
   	@products_main = Product.where(main: true).take(1)
   	@products = Product.where(public: true).take(6)
     @banners  = Banner.where(public: true)
   end
+
+  private
+    def new
+      @newsletter = Newsletter.new
+    end
 
 end
