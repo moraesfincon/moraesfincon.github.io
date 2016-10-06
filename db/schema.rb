02d93065bf1c18ed15856d7a3be900ead38629e1
Diego Collares <diego.collares@gmail.com>
2016-10-06 14:52:32 -0300

    load schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161004205734) do
+ActiveRecord::Schema.define(version: 20161005230318) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -84,6 +84,15 @@ ActiveRecord::Schema.define(version: 20161004205734) do
     t.boolean  "main",       default: false
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
+  end
+
+  create_table "soluctions", force: :cascade do |t|
+    t.string   "name"
+    t.text     "resume"
+    t.string   "image"
+    t.integer  "order"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
