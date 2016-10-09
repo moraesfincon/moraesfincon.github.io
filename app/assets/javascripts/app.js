246e1ad5cbb928c9c380f17e1ba9ebb72ef006b0
Diego Collares <diego.collares@gmail.com>
2016-10-09 17:19:08 -0300

    clear file

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,10 +1,1 @@
-"use strict";
 
-function submitForm(){
-  var form = document.getElementById('new_contact');
-  var inputs = form.elements;
-
-  for (var i = 0, len = inputs.length; i < len; ++i) {
-    inputs[i].readOnly = true;
-  }
-}
