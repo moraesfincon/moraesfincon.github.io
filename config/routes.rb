562d93fce0c70c987adaa88a57c8fd78da7eb0f2
Diego Collares <diego.collares@gmail.com>
2016-10-13 18:33:06 -0300

    add routes articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'articles/index'
+
+  get 'articles/show'
+
   mount Ckeditor::Engine => '/ckeditor'
 
   devise_for :admin_users, ActiveAdmin::Devise.config
@@ -8,6 +12,7 @@ Rails.application.routes.draw do
 
   resources :contacts, only: [:new, :create]
   resources :newsletters, only: [:new, :create]
+  resources :articles, only: [:index, :show]
 
   get 'modelos', to: 'products#index'
   get 'solucoes', to: 'soluctions#index'
