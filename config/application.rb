c94a00fd299bded8f9408d3b6b784f2c0f3501d1
Diego Collares <diego.collares@gmail.com>
2016-10-17 16:46:40 -0200

    config site-tracertag

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,12 @@ module SiteTracertag
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.assets.precompile += Ckeditor.assets
+    config.assets.precompile += %w( ckeditor/* )
+    config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
+
+
     config.i18n.default_locale = "pt-BR"
     config.time_zone = 'Brasilia'
 
