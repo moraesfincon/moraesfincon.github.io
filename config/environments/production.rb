78d78cbf13e0866221c79e4e066ca007b125b9f5
Diego Collares <diego.collares@gmail.com>
2016-10-24 18:39:28 -0200

    set config sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,6 +75,20 @@ Rails.application.configure do
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
+  config.action_mailer.smtp_settings = {
+    address: "smtp.sendgrid.net",
+    port: 587,
+    domain: "tracertag.herokuapp.com",
+    authentication: :login,
+    user_name: "senhordim",
+    enable_starttls_auto: true,
+    password: "kika1984*"
+  }
+
+  config.action_mailer.delivery_method = :smtp
+
+  routes.default_url_options = { :host => 'tracertag.herokuapp.com', protocol: 'https' }
+
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
