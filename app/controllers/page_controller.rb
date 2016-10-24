b03d892a7516ba336e091ac605e5f5612a9404cd
Diego Collares <diego.collares@gmail.com>
2016-10-24 18:21:44 -0200

    change list all products

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -4,7 +4,7 @@ class PageController < ApplicationController
 
   def index
   	@products_main = Product.where(main: true).take(1)
-    @products_launch = Product.where(launch: true).take(3)
+    @products_launch = Product.where(launch: true, public: true).take(3)
   	@products = Product.where(public: true).take(6)
     @banners  = Banner.where(public: true)
   end
