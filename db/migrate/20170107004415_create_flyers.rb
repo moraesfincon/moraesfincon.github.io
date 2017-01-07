8f1b6137ef67d8f90d085647632a779322eeb5fc
Diego Collares <diego.collares@gmail.com>
2017-01-06 22:44:51 -0200

    generate model flyer

---
+++ db/migrate/20170107004415_create_flyers.rb
@@ -0,0 +1,11 @@
+class CreateFlyers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :flyers do |t|
+      t.string :title
+      t.text :description
+      t.string :file
+
+      t.timestamps
+    end
+  end
+end
