a9b1b2679c22dc2ffe8636752e73843096e70620
Diego Collares <diego.collares@gmail.com>
2017-01-06 22:55:50 -0200

    loadschema'

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170103021435) do
+ActiveRecord::Schema.define(version: 20170107004415) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -86,6 +86,14 @@ ActiveRecord::Schema.define(version: 20170103021435) do
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "flyers", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.string   "file"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "newsletters", force: :cascade do |t|
