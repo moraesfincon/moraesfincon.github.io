6539084e5cccd04094d4683206f4ddafbfdd802a
Diego Collares <diego.collares@gmail.com>
2017-01-06 22:54:57 -0200

    validates presence of title and file

--- app/models/flyer.rb
+++ app/models/flyer.rb
@@ -1,2 +1,19 @@
+# == Schema Information
+#
+# Table name: flyers
+#
+#  id          :integer          not null, primary key
+#  title       :string
+#  description :text
+#  file        :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Flyer < ApplicationRecord
+  mount_uploader :file, FileUploader
+
+  validates_presence_of :title, :file
+
+
 end
