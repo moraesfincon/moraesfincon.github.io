56a9bb584a99850aed5845b0049c87f198aed9ad
Diego Collares <diego.collares@gmail.com>
2017-01-17 00:57:55 -0200

    change products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,7 @@
 class ProductsController < ApplicationController
 
   def index
-    @products = Product.where(public: true, launch: true ).order(:order_position )
+    @products = Product.where(public: true ).order(:order_position )
     @products_launch = Product.where(launch: true)
   end
 
