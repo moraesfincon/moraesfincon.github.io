f90a4ad8cd1b7ead2b0c9ab614ea1d2fb2b85cb5
Diego Collares <diego.collares@gmail.com>
2017-05-22 18:28:52 -0300

    update sentry keys api

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -1,4 +1,4 @@
 Raven.configure do |config|
-  config.dsn = 'https://0964a8a7a1544622aef4b475e1d87a14:a38ebab0deef4f5cb2506ac7f8fede24@sentry.io/106835'
+  config.dsn = 'https://2e6d261a1ed8423c81fe54b72bff531c:5a8db756fed94c0a91f20214b5a7ba4a@sentry.io/170973'
   config.environments = ['production']
 end
