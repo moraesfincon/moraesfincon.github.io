b11c016f2730aa78506ffa09ae8fac99c5ba1fb8
daniel.dia <daniel.dia@outlook.com>
2017-12-06 20:48:14 -0200

    alteração na quantidade de produtos na página inicial e formatação do mesmo

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -3,7 +3,7 @@ class PageController < ApplicationController
   before_action :new
 
   def index
-  	@products_main = Product.where(main: true).take(1)
+  	@products_main = Product.where(main: true).take(3)
     @products_launch = Product.where(launch: true).take(3)
   	@products = Product.where(public: true).order(order_position: :asc).take(6)
     @banners  = Banner.where(public: true)
